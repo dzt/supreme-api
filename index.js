cf4a514af5c1a92add69dc7075137d7a643023c5
Andrej Szalma <andrej@fortytwo.studio>
2020-06-30 12:40:57 +0100

    Fixed deprecated request usage, added headers

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var cheerio = require('cheerio');
 var request = require('request');
 var request = require('request').defaults({
-    //timeout: 30000
+	//timeout: 30000
 });
 
 var api = {};
 
 api.url = 'http://www.supremenewyork.com';
 
-String.prototype.capitalizeEachWord = function() {
-    return this.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-}
+String.prototype.capitalizeEachWord = function () {
+	return this.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+};
 
 // getItem('all')
 // other options: new, jackets, shirts, tops_sweaters, sweatshirts,
@@ -25,225 +25,249 @@ String.prototype.capitalizeEachWord = function() {
  * @return {Array}
  */
 
-api.getItems = function(category, callback) {
+api.getItems = function (category, callback) {
+	var getURL = api.url + '/shop/all/' + category;
+	if (category == 'all') {
+		getURL = api.url + '/shop/all';
+	} else if (category == 'new') {
+		getURL = api.url + '/shop/new';
+	}
 
-    var getURL = api.url + '/shop/all/' + category;
-    if (category == 'all') {
-        getURL = api.url + '/shop/all';
-    } else if (category == 'new') {
-        getURL = api.url + '/shop/new';
-    }
+	request(getURL, function (err, resp, html, rrr, body) {
+		if (!err) {
+			if (err) {
+				return callback('No response from website', null);
+			} else {
+				var $ = cheerio.load(html);
+			}
 
-    request(getURL, function(err, resp, html, rrr, body) {
+			var count = $('img').length;
 
-        if (!err) {
-            if (err) {
-                console.log('err')
-                return callback('No response from website', null);
-            } else {
-                var $ = cheerio.load(html);
-            }
+			if ($('.shop-closed').length > 0) {
+				return callback('Store Closed', null);
+			} else if (count === 0) {
+				return callback('Store Closed', null);
+			}
 
-            var count = $('img').length;
+			var parsedResults = [];
 
-            if ($('.shop-closed').length > 0) {
-              return callback('Store Closed', null);
-            } else if (count === 0) {
-              return callback('Store Closed', null);
-            }
+			$('img').each(function (i, element) {
+				var nextElement = $(this).next();
+				var prevElement = $(this).prev();
+				var image = 'https://' + $(this).attr('src').substring(2);
+				var title = $(this).attr('alt');
+				var availability = nextElement.text().capitalizeEachWord();
+				var link = api.url + this.parent.attribs.href;
+				var sizesAvailable;
 
-            var parsedResults = [];
+				if (availability == '') availability = 'Available';
 
+				request(
+					{
+						url: link,
+						headers: {
+							accept: 'text/html',
+						},
+					},
+					function (err, resp, html, rrr, body) {
+						if (err) {
+							return callback('No response from website', null);
+						} else {
+							var $ = cheerio.load(html);
+						}
 
-            // console.log(len);
-            $('img').each(function(i, element) {
+						var addCartURL = api.url + $('form[id="cart-addf"]').attr('action');
 
-                var nextElement = $(this).next();
-                var prevElement = $(this).prev();
-                var image = "https://" + $(this).attr('src').substring(2);
-                var title = $(this).attr('alt');
-                var availability = nextElement.text().capitalizeEachWord();
-                var link = api.url + this.parent.attribs.href;
-                var sizesAvailable;
+						if (availability == 'Sold Out') {
+							addCartURL = null;
+						}
 
+						var sizeOptionsAvailable = [];
+						if ($('option')) {
+							$('option').each(function (i, elem) {
+                                var size = {
+                                    id: parseInt($(this).attr('value')),
+                                    size: $(this).text(),
+                                };
+                                sizeOptionsAvailable.push(size);
+							});
 
-                if (availability == "") availability = "Available";
+							if (sizeOptionsAvailable.length > 0) {
+                                // sizesAvailable = sizeOptionsAvailable.map((item) => {return item.size+":"+item.id})
+                                sizesAvailable = sizeOptionsAvailable;
+							} else {
+                                sizesAvailable = null;
+							}
+						} else {
+                            sizesAvailable = null;
+						}
+                            
+						var metadata = {
+							title: $('h1').attr('itemprop', 'name').eq(1).html(),
+							style: $('.style').attr('itemprop', 'model').text(),
+							link: link,
+							description: $('.description').text(),
+							addCartURL: addCartURL,
+							price: $('.price')[0].children[0].children[0].data.match(
+								/(\d+)/
+							)[0],
+							image: image,
+							sizesAvailable: sizesAvailable,
+							images: [],
+							availability: availability,
+						};
 
-                request(link, function(err, resp, html, rrr, body) {
+						// Some items don't have extra images (like some of the skateboards)
+						if ($('.styles').length > 0) {
+							var styles = $('.styles')[0].children;
+							for (li in styles) {
+								for (a in styles[li].children) {
+									if (
+										styles[li].children[a].attribs['data-style-name'] ==
+										metadata.style
+									) {
+										metadata.images.push(
+											'https:' +
+												JSON.parse(
+													styles[li].children[a].attribs['data-images']
+												).zoomed_url
+										);
+									}
+								}
+							}
+						} else if (title.indexOf('Skateboard') != -1) {
+							// Because fuck skateboards
+							metadata.images.push('https:' + $('#img-main').attr('src'));
+						}
 
-                    if (err) {
-                        return callback('No response from website', null);
-                    } else {
-                        var $ = cheerio.load(html);
-                    }
+						parsedResults.push(metadata);
 
-                    var addCartURL = api.url + $('form[id="cart-addf"]').attr('action');
-
-                    if (availability == "Sold Out") {
-                        addCartURL = null;
-                    }
-
-                    var sizeOptionsAvailable = [];
-                    if ($('option')) {
-                        $('option').each(function(i, elem) {
-                            var size = {
-                                id: parseInt($(this).attr('value')),
-                                size: $(this).text(),
-                            }
-                            sizeOptionsAvailable.push(size);
-                        });
-
-                        if (sizeOptionsAvailable.length > 0) {
-                            sizesAvailable = sizeOptionsAvailable
-                        } else {
-                            sizesAvailable = null
-                        }
-                    } else {
-                        sizesAvailable = null;
-                    }
-
-                    var metadata = {
-                        title: $('h1').attr('itemprop', 'name').eq(1).html(),
-                        style: $('.style').attr('itemprop', 'model').text(),
-                        link: link,
-                        description: $('.description').text(),
-                        addCartURL: addCartURL,
-                        price: parseInt(($('.price')[0].children[0].children[0].data).replace('$', '').replace(',', '')),
-                        image: image,
-                        sizesAvailable: sizesAvailable,
-                        images: [],
-                        availability: availability
-                    };
-
-                    // Some items don't have extra images (like some of the skateboards)
-                    if ($('.styles').length > 0) {
-                        var styles = $('.styles')[0].children;
-                        for (li in styles) {
-                            for (a in styles[li].children) {
-                                if (styles[li].children[a].attribs['data-style-name'] == metadata.style) {
-                                    metadata.images.push('https:' + JSON.parse(styles[li].children[a].attribs['data-images']).zoomed_url)
-                                }
-                            }
-                        }
-                    } else if (title.indexOf('Skateboard') != -1) {
-                        // Because fuck skateboards
-                        metadata.images.push('https:' + $('#img-main').attr('src'))
-                    }
-
-                    parsedResults.push(metadata);
-
-                    if (!--count) {
-                        callback(parsedResults, null);
-                    }
-
-                })
-
-            });
-        } else {
-            return callback('No response from website', null);
-        }
-    });
+						if (!--count) {
+							callback(parsedResults, null);
+						}
+					}
+				);
+			});
+		} else {
+			return callback('No response from website', null);
+		}
+	});
 };
 
-api.getItem = function(itemURL, callback) {
+api.getItem = function (itemURL, callback) {
+	request(
+		{
+			url: itemURL,
+			headers: { accept: 'text/html' },
+		},
+		function (err, resp, html, rrr, body) {
+			if (err) {
+				return callback('No response from website', null);
+			} else {
+				var $ = cheerio.load(html);
+			}
 
-    request(itemURL, function(err, resp, html, rrr, body) {
+			var sizeOptionsAvailable = [];
+			if ($('option')) {
+				$('option').each(function (i, elem) {
+					var size = {
+						id: parseInt($(this).attr('value')),
+						size: $(this).text(),
+					};
+					sizeOptionsAvailable.push(size);
+				});
 
-        if (err) {
-            return callback('No response from website', null);
-        } else {
-            var $ = cheerio.load(html);
-        }
+				if (sizeOptionsAvailable.length > 0) {
+					sizesAvailable = sizeOptionsAvailable;
+				} else {
+					sizesAvailable = null;
+				}
+			} else {
+				sizesAvailable = null;
+			}
 
-        var sizeOptionsAvailable = [];
-        if ($('option')) {
-            $('option').each(function(i, elem) {
-                var size = {
-                    id: parseInt($(this).attr('value')),
-                    size: $(this).text(),
-                }
-                sizeOptionsAvailable.push(size);
-            });
+			var availability;
+            var addCartURL = api.url + $('form[id="cart-addf"]').attr('action');
 
-            if (sizeOptionsAvailable.length > 0) {
-                sizesAvailable = sizeOptionsAvailable
-            } else {
-                sizesAvailable = null
-            }
-        } else {
-            sizesAvailable = null;
-        }
+            var addCartButton = $('input[type=submit][name=commit]')[0];
 
-        var availability;
-        var addCartURL = api.url + $('form[id="cart-addf"]').attr('action');
+			if (addCartButton !== undefined) {
+                availability = 'Available';
+			} else {
+				availability = 'Sold Out';
+			}
 
-        var addCartButton = $('input[value="add to cart"]')
-        if (addCartButton.attr('type') == '') {
-            availability = 'Available'
-        } else {
-            availability = 'Sold Out'
-        }
+			if (availability == 'Sold Out') {
+				addCartURL = null;
+			}
 
-        if (availability == 'Sold Out') {
-            addCartURL = null
-        }
+			var metadata = {
+				title: $('h1').attr('itemprop', 'name').eq(1).html(),
+				style: $('.style').attr('itemprop', 'model').text(),
+				link: itemURL,
+				description: $('.description').text(),
+				addCartURL: addCartURL,
+                price: $('.price')[0].children[0].children[0].data.match(
+                    /(\d+)/
+                )[0],
+                image: 'http:' + $('#img-main').attr('src'),
+				sizesAvailable: sizesAvailable,
+				images: [],
+				availability: availability,
+			};
 
-        var metadata = {
-            title: $('h1').attr('itemprop', 'name').eq(1).html(),
-            style: $('.style').attr('itemprop', 'model').text(),
-            link: itemURL,
-            description: $('.description').text(),
-            addCartURL: addCartURL,
-            price: parseInt(($('.price')[0].children[0].children[0].data).replace('$', '').replace(',', '')),
-            image: 'http:' + $('#img-main').attr('src'),
-            sizesAvailable: sizesAvailable,
-            images: [],
-            availability: availability
-        };
+			// Some items don't have extra images (like some of the skateboards)
+			if ($('.styles').length > 0) {
+				var styles = $('.styles')[0].children;
+				for (li in styles) {
+					for (a in styles[li].children) {
+						if (
+							styles[li].children[a].attribs['data-style-name'] ==
+							metadata.style
+						) {
+							metadata.images.push(
+								'https:' +
+									JSON.parse(styles[li].children[a].attribs['data-images'])
+										.zoomed_url
+							);
+						}
+					}
+				}
+			}
+			else if (metadata.title.indexOf('Skateboard') != -1) {
+			    metadata.images.push('https:' + $('#img-main').attr('src'))
+			}
 
-        // Some items don't have extra images (like some of the skateboards)
-        if ($('.styles').length > 0) {
-            var styles = $('.styles')[0].children;
-            for (li in styles) {
-                for (a in styles[li].children) {
-                    if (styles[li].children[a].attribs['data-style-name'] == metadata.style) {
-                        metadata.images.push('https:' + JSON.parse(styles[li].children[a].attribs['data-images']).zoomed_url)
-                    }
-                }
-            }
-        } else if (title.indexOf('Skateboard') != -1) {
-            metadata.images.push('https:' + $('#img-main').attr('src'))
-        }
-
-        callback(null, metadata);
-    });
+			callback(null, metadata);
+		}
+	);
 };
 
 api.watchOnAllItems = [];
-api.watchAllItems = function(interval, category, callback) {
-    api.log('Now watching for all items');
-    api.watchOnAllItems = setInterval(function() {
-        api.getItems(category, function(items) {
-            callback(items, null);
-        });
-    }, 1000 * interval); // Every xx sec
-}
+api.watchAllItems = function (interval, category, callback) {
+	api.log('Now watching for all items');
+	api.watchOnAllItems = setInterval(function () {
+		api.getItems(category, function (items) {
+			callback(items, null);
+		});
+	}, 1000 * interval); // Every xx sec
+};
 
-api.stopWatchingAllItems = function(callback) {
-    clearInterval(api.watchOnAllItems);
-    if (api.watchOnAllItems == "") {
-        callback(null, 'No watching processes found.');
-    } else {
-        callback('Watching has stopped.', null);
-    }
-}
+api.stopWatchingAllItems = function (callback) {
+	clearInterval(api.watchOnAllItems);
+	if (api.watchOnAllItems == '') {
+		callback(null, 'No watching processes found.');
+	} else {
+		callback('Watching has stopped.', null);
+	}
+};
 
 // searches for new item drop TODO
-api.onNewItem = function(callback) {
-    api.watchAllItems(function(item) {
-        // TODO: If new items is find return callback(value);
-    });
-}
+api.onNewItem = function (callback) {
+	api.watchAllItems(function (item) {
+		// TODO: If new items is find return callback(value);
+	});
+};
 
 /**
  * Seeks for items on desired category page with specific keywords/styles.
@@ -253,50 +277,53 @@ api.onNewItem = function(callback) {
  * @param  {String} category
  * @return {Object}
  */
-api.seek = function(category, keywords, styleSelection, callback) {
-    var productLink = [];
-    api.getItems(category, (product, err) => {
+api.seek = function (category, keywords, styleSelection, callback) {
+	var productLink = [];
+	api.getItems(category, (product, err) => {
+		if (err) {
+			return callback(null, 'Error occured while trying to seek for items.');
+		}
 
-        if (err) {
-            return callback(null, 'Error occured while trying to seek for items.');
-        }
+		for (i = 0; i < product.length; i++) {
+			var title = product[i].title;
+			var style = product[i].style;
 
-        for (i = 0; i < product.length; i++) {
-            var title = product[i].title;
-            var style = product[i].style;
+			if (style === null) {
+				// type - style not defined without a match
+				if (title.indexOf(keywords) > -1) {
+					// check if the keywords match with the title
+					// found item
+					productLink.push(product[i].link);
+					return callback(product[i], null);
+					break;
+				} else {
+					continue;
+				}
+			} else if (style.indexOf(styleSelection) > -1) {
+				// type - style defined with match
+				if (title.indexOf(keywords) > -1) {
+					// check if the keywords match with the title
+					// found item
+					productLink.push(product[i].link);
+					return callback(product[i], null);
+					break;
+				} else {
+					continue;
+				}
+			}
+		}
 
-            if (style === null) {
-                // type - style not defined without a match
-                if (title.indexOf(keywords) > -1) { // check if the keywords match with the title
-                    // found item
-                    productLink.push(product[i].link);
-                    return callback(product[i], null);
-                    break;
-                } else {
-                    continue;
-                }
-            } else if (style.indexOf(styleSelection) > -1) {
-                // type - style defined with match
-                if (title.indexOf(keywords) > -1) { // check if the keywords match with the title
-                    // found item
-                    productLink.push(product[i].link);
-                    return callback(product[i], null);
-                    break;
-                } else {
-                    continue;
-                }
-            }
-        }
+		if (productLink[0] === undefined) {
+			return callback(
+				null,
+				'Could not find any results matching your keywords.'
+			);
+		}
+	});
+};
 
-        if (productLink[0] === undefined) {
-            return callback(null, "Could not find any results matching your keywords.");
-        }
-
-    });
-}
-
-api.log = function(message) {
-    console.log('[supreme api] ' + message);
-}
+api.log = function (message) {
+	console.log('[supreme api] ' + message);
+};
 
 module.exports = api;
