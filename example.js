86a93c7fe7660caec920f4066063dc9695cb3756
Andrej Szalma <andrej@fortytwo.studio>
2020-06-30 12:50:31 +0100

    Updated contents of example to work with current web content

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
 var supreme = require('./index') || require('supreme-api');
 
 // Find Product Based on Keywords
-const keywords = "Small Box";
+const keywords = 'Nike';
 const style = 'White';
-const category = 'new';
+const category = 'shoes';
 
 // supreme.seek(category, keywords, style, (product, err) => {
 //     if (err) {
@@ -13,14 +13,13 @@ const category = 'new';
 //     console.log(product);
 // });
 
-
-supreme.getItems('shoes', (product, err) => {
-    if (err) {
-        console.log(err);
-        return err;
-    }
-    console.log(product);
-});
+// supreme.getItems('shoes', (product, err) => {
+//     if (err) {
+//         console.log(err);
+//         return err;
+//     }
+//     console.log(product);
+// });
 
 // supreme.getItem('http://www.supremenewyork.com/shop/shoes/rkxgtf1n2/rgrx634kb', (item, err) => {
 //      if (err) {
@@ -29,3 +28,11 @@ supreme.getItems('shoes', (product, err) => {
 //      }
 //      //console.log(item);
 //  });
+
+// supreme.onNewItem(5, "all", (items, err) => {
+// 	if (err) {
+// 		console.log(err);
+// 		return err;
+// 	}
+//     console.log(items)
+// });
